b0ae5154014664a585d864a64bef1d0409b6fb50
Yashraj1939x <deshmukhyashraj670@gmail.com>
2026-01-03 14:25:55 +0530

    Update moneyQuiz.js

--- js/moneyQuiz.js
+++ js/moneyQuiz.js
@@ -15,25 +15,62 @@ window.calculateScore = function () {
 
   let personality = "";
   let colorClass = "";
+  let traits = "";
 
   if (totalScore <= 9) {
     personality = "Spender";
     colorClass = "spender";
+    traits = `
+      <h4>Traits Of Spender</h4>
+      <ul>
+        <li>Enjoys spending money</li>
+        <li>Lives in the moment</li>
+        <li>Trouble saving and planning</li>
+        <li>Might struggle with debt</li>
+      </ul>
+    `;
   } else if (totalScore <= 14) {
     personality = "Balancer";
     colorClass = "balancer";
+    traits = `
+      <h4>Traits Of Balancer</h4>
+      <ul>
+        <li>Good at managing money</li>
+        <li>Prone to indecision</li>
+        <li>May miss out on good opportunities</li>
+      </ul>
+    `;
   } else if (totalScore <= 19) {
     personality = "Saver";
     colorClass = "saver";
+    traits = `
+      <h4>Traits Of Saver</h4>
+      <ul>
+        <li>Excellent at saving money</li>
+        <li>May be frugal or rigid</li>
+        <li>May neglect personal needs or wants</li>
+        <li>Trouble sharing money with others</li>
+      </ul>
+    `;
   } else {
     personality = "Investor";
     colorClass = "investor";
+    traits = `
+      <h4>Traits Of Investor</h4>
+      <ul>
+        <li>Savvy and strategic</li>
+        <li>Seeks to grow wealth and positive impact</li>
+        <li>Adventurous and willing to take risks</li>
+        <li>Sometimes optimistic or ignorant of risks</li>
+      </ul>
+    `;
   }
 
   resultDiv.style.display = "block";
   resultDiv.innerHTML = `
     <h3 class="${colorClass}">You are a ${personality}</h3>
     <p class="score">Your Score: ${totalScore}</p>
+    ${traits}
   `;
 
   resultDiv.scrollIntoView({ behavior: "smooth" });
