9455228d6ec40ae08928ec395f08e344cf476ee0
Yashraj1939x <deshmukhyashraj670@gmail.com>
2026-01-04 13:39:16 +0530

    Update banking.js

--- banking/banking.js
+++ banking/banking.js
@@ -10,15 +10,3 @@ function openTab(tabId, event) {
   document.getElementById(tabId).classList.add("active");
   event.target.classList.add("active");
 }
-function openTab(tabId, event) {
-  document.querySelectorAll(".tab-content").forEach(section => {
-    section.classList.remove("active");
-  });
-
-  document.querySelectorAll(".tab-btn").forEach(btn => {
-    btn.classList.remove("active");
-  });
-
-  document.getElementById(tabId).classList.add("active");
-  event.target.classList.add("active");
-}
