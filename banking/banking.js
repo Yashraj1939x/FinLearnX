3e9182d72d56ff3044e16e604842120451974e58
Yashraj1939x <deshmukhyashraj670@gmail.com>
2026-01-04 13:17:57 +0530

    Create banking.js

---
+++ banking/banking.js
@@ -0,0 +1,10 @@
+function openTab(tabId) {
+  const contents = document.querySelectorAll(".tab-content");
+  const buttons = document.querySelectorAll(".tab-btn");
+
+  contents.forEach(content => content.classList.remove("active"));
+  buttons.forEach(btn => btn.classList.remove("active"));
+
+  document.getElementById(tabId).classList.add("active");
+  event.target.classList.add("active");
+}
